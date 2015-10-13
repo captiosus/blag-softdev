3b4e44988db2dbb71b40f64fd1b4bcf4094df322
shishu1998 <shishu_1998@yahoo.com>
2015-10-13 17:36:18 -0400

    Changed to users instead of posts

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import sqlite3
 def authenticate(username,password):
     conn = sqlite3.connect("blag.db")
     c = conn.cursor()
-    q = "SELECT password from posts WHERE username == %s"%username 
+    q = "SELECT password from users WHERE username == %s"%username 
     c.execute(q)
     result = c.fetchall()
     return result == password
