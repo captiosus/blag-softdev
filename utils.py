2cb1327f394f3567ea957348678624fadee04e8c
Shi Shu <shishu_1998@yahoo.com>
2015-10-13 12:34:27 -0400

    small stuff

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ def authenticate(username,password):
     conn = sqlite3.connect("blag.db")
     c = conn.cursor()
     q = "SELECT password from posts WHERE username == %s"%username 
-    result = c.execute(q)
+    c.execute(q)
+    result = c.fetchall()
     return result == password
     
