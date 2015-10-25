13dc0dfcd457e1bb17709c13b7f7dc60d306d312
RJZheng1 <stuykid2016@yahoo.com>
2015-10-24 21:09:46 -0400

    Did some more changes.

--- utils.py
+++ utils.py
@@ -1,164 +1,116 @@
 from pymongo import MongoClient
 import hashlib
-import time
+from datetime import datetime
 
-error = ""
 connection = MongoClient()
 db = connection['blog']
 
 def authenticate(username,password):
-    result = db.blog.find({'name':username})
-    global error
+    result = db.user.find({'username':username})
     if len(result) == 0:
-        error = "User does not exist"
-        return False
-else:
-    if pw['password'] != hashlib.sha224(password).hexdigest():
-        error = "Password does not match username"
-        return False
-else:
-    db.blog.update({'name':username}, {$set:{'time':currentTime()}})
-    return True
-
-def getError():
-    global error
-    return  error
-
-def currentTime():
-    return (time.strftime("%m/%d/%Y")) + " " + (time.strftime("%H:%M:%S"))
+        return "User does not exist"
+    else:
+        if pw['password'] != hashlib.sha224(password).hexdigest():
+            return "Incorrect password"
+        else:
+            db.user.update({'username':username}, {'$set':{'time':datetime.now()}})
+            return None
 
 def getTime(username):
-    conn = sqlite3.connect("blag.db")
-    c = conn.cursor()
-    q = "SELECT timestamp from users WHERE username=:uname"
-    c.execute(q,{"uname":username})
-    result = c.fetchone()
-    if result == None:
-        r = "UPDATE users SET timestamp =:time WHERE username=:uname"
-        c.execute(r,{"time":currentTime(),"uname":username})
-        return "Never logged in before"
-else:
-    time = result[0]
-    r = "UPDATE users SET timestamp =:time WHERE username=:uname"
-    c.execute(r,{"time":currentTime(),"uname":username})
-    return time;
-
-
-def nextuserid():
-    conn = sqlite3.connect('blag.db')
-    cur = conn.cursor()
-    cur.execute('SELECT MAX(userid) FROM users')
-    userid = cur.fetchall()
-    cur.close()
-    if userid[0] is None:
-        return 1
-    return userid[0][0]+1
-
-def nextpostid():
-    conn = sqlite3.connect('blag.db')
-    cur = conn.cursor()
-    cur.execute('SELECT MAX(postid) FROM posts')
-    postid = cur.fetchall()
-    cur.close()
-    print postid
-    if postid[0][0] is None:
-        return 1
-    return postid[0][0]+1
+    result = db.user.find({'username':username})
+    if len(result) != 0:
+        time = result['time']
+        db.user.update({'username':username}, {'$set':{'time':datetime..now()}})
+        return time;
+    return None
 
 def createuser(username,password):
     user = {}
-    
-    time = currentTime()
-    db.blog.insert(
-        cur.execute('INSERT INTO users(userid,username,password,timestamp) VALUES(?,?,?,?)',(newuserid,username,password,time))
-        conn.commit()
-        cur.close()
+    user['username'] = username
+    user['password'] = password
+    user['time'] = datetime.now()
+    db.user.insert(user)
 
 def createpost(newpostid,username,post):
-        conn = sqlite3.connect('blag.db')
-        cur = conn.cursor()
-        cur.execute('INSERT INTO posts(postid,username,post,timestamp) VALUES(?,?,?,?)',(newpostid,username,post,currentTime()))
-        conn.commit()
-        cur.close()
+    conn = sqlite3.connect('blag.db')
+    cur = conn.cursor()
+    cur.execute('INSERT INTO posts(postid,username,post,timestamp) VALUES(?,?,?,?)',(newpostid,username,post,currentTime()))
+    conn.commit()
+    cur.close()
 
 def deletepost(postid):
-        conn = sqlite3.connect('blag.db')
-        cur = conn.cursor()
-        cur.execute('DELETE FROM posts WHERE postid=:id',{"id":postid})
-        cur.execute('DELETE FROM comments WHERE postid=:id',{"id":postid})
-        conn.commit()
-        cur.close()
+    conn = sqlite3.connect('blag.db')
+    cur = conn.cursor()
+    cur.execute('DELETE FROM posts WHERE postid=:id',{"id":postid})
+    cur.execute('DELETE FROM comments WHERE postid=:id',{"id":postid})
+    conn.commit()
+    cur.close()
 
 def editpost(postid,username,post):
-        conn = sqlite3.connect('blag.db')
-        cur = conn.cursor()
-        cur.execute('UPDATE posts SET username = ? WHERE postid = ?',(username,postid))
-        cur.execute('UPDATE posts SET post = ? WHERE postid = ?',(post,postid))
-        cur.execute('UPDATE posts SET timestamp = ? WHERE postid = ?',(currentTime(),postid))
-        conn.commit()
-        cur.close()
+    db.blog.update({'postid':postid}, {'$set':{'username':username}})
+    db.blog.update({'postid':postid}, {'$set':{'post':post}})
+    db.blog.update({'postid':postid}, {'$set':{'time':datetime.datetime.now()}})
 
 def displayposts():
-        conn = sqlite3.connect('blag.db')
-        cur = conn.cursor()
-        cur.execute('SELECT postid, timestamp, post, username FROM posts')
-        allposts = cur.fetchall()
-        postscomments = []
-        for post in allposts:
+    conn = sqlite3.connect('blag.db')
+    cur = conn.cursor()
+    cur.execute('SELECT postid, timestamp, post, username FROM posts')
+    allposts = cur.fetchall()
+    postscomments = []
+    for post in allposts:
         postid = post[0]
         cur.execute('SELECT commentid, comment, username FROM comments WHERE postid=:id',{"id":postid})
         comments = cur.fetchall()
         comments = tuple(comments)
         post = post + comments
         postscomments.append(post)
-        cur.close()
-        return postscomments
+    cur.close()
+    return postscomments
 
 def getpost(postid):
-        conn = sqlite3.connect('blag.db')
-        cur = conn.cursor()
-        cur.execute('SELECT post FROM posts WHERE postid=:id',{"id":postid})
-        post = cur.fetchone()[0]
-        cur.close()
-        return post
+    conn = sqlite3.connect('blag.db')
+    cur = conn.cursor()
+    cur.execute('SELECT post FROM posts WHERE postid=:id',{"id":postid})
+    post = cur.fetchone()[0]
+    cur.close()
+    return post
 
 def nextcommentid(postid):
-        conn = sqlite3.connect('blag.db')
-        cur = conn.cursor()
-        cur.execute('SELECT MAX(commentid) FROM comments WHERE postid=:id',{"id":postid})
-        commentid = cur.fetchone()
-        cur.close()
-        if commentid[0] is None:
+    conn = sqlite3.connect('blag.db')
+    cur = conn.cursor()
+    cur.execute('SELECT MAX(commentid) FROM comments WHERE postid=:id',{"id":postid})
+    commentid = cur.fetchone()
+    cur.close()
+    if commentid[0] is None:
         return 1
-        return commentid[0]+1
+    return commentid[0]+1
 
 def createcomment(postid,newcommentid,username,comment):
-        conn = sqlite3.connect('blag.db')
-        cur = conn.cursor()
-        cur.execute('INSERT INTO comments(postid,commentid,username,comment,timestamp) VALUES(?,?,?,?,?)',(postid,newcommentid,username,comment,currentTime()))
-        conn.commit()
-        cur.close()
+    conn = sqlite3.connect('blag.db')
+    cur = conn.cursor()
+    cur.execute('INSERT INTO comments(postid,commentid,username,comment,timestamp) VALUES(?,?,?,?,?)',(postid,newcommentid,username,comment,currentTime()))
+    conn.commit()
+    cur.close()
 
 def finduserposts(username):
-        conn = sqlite3.connect('blag.db')
-        cur = conn.cursor()
-        cur.execute('SELECT postid, timestamp, post, username FROM posts where username=:uname',{"uname":username})
-        allposts = cur.fetchall()
-        postscomments = []
-        for post in allposts:
+    conn = sqlite3.connect('blag.db')
+    cur = conn.cursor()
+    cur.execute('SELECT postid, timestamp, post, username FROM posts where username=:uname',{"uname":username})
+    allposts = cur.fetchall()
+    postscomments = []
+    for post in allposts:
         postid = post[0]
         cur.execute('SELECT commentid, comment, username FROM comments WHERE postid=:id',{"id":postid})
         comments = cur.fetchall()
         comments = tuple(comments)
         post = post + comments
         postscomments.append(post)
-        cur.close()
-        return postscomments
-
+    cur.close()
+    return postscomments
 
 def is_number(s):
-        try:
+    try:
         float(s)
         return True
-        except ValueError:
+    except ValueError:
         return False
